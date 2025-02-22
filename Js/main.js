46536dc438ede1f1a56511e1a38159dcf378285c
Apdla-coder <apdlahany@gmail.com>
2025-02-22 13:48:26 +0200

    First commit

---
+++ Js/main.js
@@ -0,0 +1,20 @@
+// var y = "33";
+// var x = "7";
+// console.log(y % x);
+// var y = "apdla_";
+// var x = "hany";
+// console.log(y + x); 
+var input1 = window.prompt("enter the valu");
+var input3 = window.prompt("enter the valu");
+// var input4 = window.prompt("enter the valu");
+// console.log(Number(input1) + Number(input2));
+// console.log(Number(input3) - Number(input4));
+if (input1 == "red" && input2 == "ff") {
+    console.log("hello");
+}
+else if (input1 == "blue") {
+    console.log("yes");
+}
+else {
+    console.log("no");
+}
